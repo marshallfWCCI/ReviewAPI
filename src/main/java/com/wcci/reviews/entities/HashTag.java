f2b8b5187966fb1f00a7c9e460fedc38b84f392c
Marshall F <marshall@wecancodeit.org>
2023-02-11 16:54:16 -0500

    Lets add a field to HashTag

--- src/main/java/com/wcci/reviews/entities/HashTag.java
+++ src/main/java/com/wcci/reviews/entities/HashTag.java
@@ -1,13 +1,21 @@
 package com.wcci.reviews.entities;
 
+import java.time.LocalDateTime;
+
 public class HashTag {
     final private String name;
+    final private LocalDateTime creationTime;
 
     public HashTag(final String name) {
         this.name = name;
+        this.creationTime = LocalDateTime.now();
     }
 
     public String getName() {
         return name;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
     }
 }
--- src/test/java/com/wcci/reviews/entities/HashTagTest.java
+++ src/test/java/com/wcci/reviews/entities/HashTagTest.java
@@ -2,6 +2,8 @@ package com.wcci.reviews.entities;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HashTagTest {
@@ -11,4 +13,13 @@ class HashTagTest {
         assertEquals("Recommended", hashTag.getName());
     }
 
+    @Test
+    public void testCreation() {
+        final LocalDateTime before = LocalDateTime.now();
+        final HashTag hashTag = new HashTag("Recommended");
+        final LocalDateTime after = LocalDateTime.now();
+
+        assertFalse(before.isAfter(hashTag.getCreationTime()));
+        assertFalse(hashTag.getCreationTime().isAfter(after));
+    }
 }
