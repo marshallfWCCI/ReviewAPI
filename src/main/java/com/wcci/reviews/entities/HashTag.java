d657d6e42f7ce4fcb697548a2fc055e522b24f57
Marshall F <marshall@wecancodeit.org>
2023-02-11 16:59:18 -0500

    Make HashTag a JPA Entity

--- src/main/java/com/wcci/reviews/entities/HashTag.java
+++ src/main/java/com/wcci/reviews/entities/HashTag.java
@@ -1,10 +1,18 @@
 package com.wcci.reviews.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class HashTag {
-    final private String name;
-    final private LocalDateTime creationTime;
+    @Id
+    private String name;
+    private LocalDateTime creationTime;
+
+    // Required by JPA
+    protected HashTag() {
+    }
 
     public HashTag(final String name) {
         this.name = name;
