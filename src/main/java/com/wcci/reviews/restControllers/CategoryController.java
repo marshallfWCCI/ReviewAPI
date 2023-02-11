22114798ded1d7864be794bcfaee54a703021c53
Marshall F <marshall@wecancodeit.org>
2023-02-11 18:43:50 -0500

    First unit test

--- src/main/java/com/wcci/reviews/restControllers/CategoryController.java
+++ src/main/java/com/wcci/reviews/restControllers/CategoryController.java
@@ -1,7 +1,21 @@
 package com.wcci.reviews.restControllers;
 
+import com.wcci.reviews.entities.Category;
+import com.wcci.reviews.respositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+    final CategoryRepository categoryRepository;
+
+    public CategoryController(final @Autowired CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/categories")
+    public Iterable<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
 }
