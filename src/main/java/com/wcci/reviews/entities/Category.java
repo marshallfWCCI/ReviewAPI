b3434cd38ac3a4c940e338588d8dd2b6c81cec86
Marshall F <marshall@wecancodeit.org>
2023-02-11 16:35:57 -0500

    Switch to MySQL, and enable useful logging, etc. Test with first table

--- src/main/java/com/wcci/reviews/entities/Category.java
+++ src/main/java/com/wcci/reviews/entities/Category.java
@@ -1,12 +1,22 @@
 package com.wcci.reviews.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity()
 public class Category {
-    final private String categoryName;
-    final private String categoryDescription;
+    @Id
+    private String categoryName;
+    private String categoryDescription;
 
     public Category(final String categoryName, final String categoryDescription) {
         this.categoryName = categoryName;
         this.categoryDescription = categoryDescription;
+    }
+
+    // JPA requires a public or protected zero-argument constructor
+    protected Category() {
     }
 
     public String getName() {
