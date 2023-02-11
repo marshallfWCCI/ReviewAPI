ff5137238f3ac444dc71d1297d2abf567b70e0e5
Marshall F <marshall@wecancodeit.org>
2023-02-11 15:38:25 -0500

    Reviews contain tags

--- src/main/java/com/wcci/reviews/entities/Review.java
+++ src/main/java/com/wcci/reviews/entities/Review.java
@@ -1,10 +1,15 @@
 package com.wcci.reviews.entities;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Review {
     final private Category category;
     final private String title;
     final private String author;
     final private String text;
+    final private Set<HashTag> tags = new HashSet<>();
 
     public Review(final Category category, final String title, final String author, final String text) {
         this.category = category;
@@ -29,4 +34,11 @@ public class Review {
         return text;
     }
 
+    public void addTag(final HashTag tag) {
+        tags.add(tag);
+    }
+
+    public Collection<HashTag> getTags() {
+        return tags;
+    }
 }
--- src/test/java/com/wcci/reviews/entities/ReviewTest.java
+++ src/test/java/com/wcci/reviews/entities/ReviewTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReviewTest {
     @Test
-    public void test() {
+    public void testConstructor() {
         final Category category = new Category("Movie");
         final String title = "Princess Bride";
         final String author = "William Golding";
@@ -17,6 +17,23 @@ class ReviewTest {
         assertEquals(title, review.getTitle());
         assertEquals(author, review.getAuthor());
         assertEquals(text, review.getText());
+    }
+
+    @Test
+    public void testTags() {
+        final Category category = new Category("Movie");
+        final String title = "Princess Bride";
+        final String author = "William Golding";
+        final String text = "Entertaining on many levels";
+        final Review review = new Review(category, title, author, text);
+
+        final HashTag tag1 = new HashTag("For kids");
+        final HashTag tag2 = new HashTag("For adults");
+        review.addTag(tag1);
+        review.addTag(tag2);
+        assertTrue(review.getTags().contains(tag1));
+        assertTrue(review.getTags().contains(tag2));
+        assertEquals(2, review.getTags().size());
     }
 
 }
