577c28136d25c23fbeb81e01166d30852d672860
Marshall F <marshall@wecancodeit.org>
2023-02-12 10:19:02 -0500

    Add two templates

--- src/test/java/com/wcci/reviews/restControllers/CategoryControllerTest.java
+++ src/test/java/com/wcci/reviews/restControllers/CategoryControllerTest.java
@@ -38,4 +38,21 @@ public class CategoryControllerTest {
                         .content(new ObjectMapper().writeValueAsString(category)))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void addCategories() throws Exception {
+        final Category category1 = new Category("Romance", "Happily-ever-after");
+        final Category category2 = new Category("Climatology", "*Not* Happily-ever-after");
+
+        mvc.perform(MockMvcRequestBuilders.post("/categories")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(category1)))
+                .andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.post("/categories")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(category2)))
+                .andExpect(status().isOk());
+    }
 }
