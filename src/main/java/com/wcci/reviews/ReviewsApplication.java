06deaf8b7d1ac468a7ca9ffbefbcc31942e845ae
Marshall F <marshall@wecancodeit.org>
2023-02-14 14:00:04 -0500

    Log HTTP traffic in test

--- src/main/java/com/wcci/reviews/ReviewsApplication.java
+++ src/main/java/com/wcci/reviews/ReviewsApplication.java
@@ -2,6 +2,8 @@ package com.wcci.reviews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class ReviewsApplication {
@@ -10,4 +12,13 @@ public class ReviewsApplication {
 		SpringApplication.run(ReviewsApplication.class, args);
 	}
 
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setMaxPayloadLength(64000);
+		return loggingFilter;
+	}
 }
