361f8999fe470eebbc5ac594c9c4494f528c524a
Marshall F <marshall@wecancodeit.org>
2023-02-16 11:40:14 -0500

    More comments

--- src/main/java/com/wcci/reviews/ReviewsApplication.java
+++ src/main/java/com/wcci/reviews/ReviewsApplication.java
@@ -9,10 +9,15 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 public class ReviewsApplication {
 
 	public static void main(String[] args) {
-		// Hey, SpringApplication, do the needful for everything in the ReviewsApplication class
+		// Hey, SpringApplication, do the needful for everything in the ReviewsApplication class.
+		// For example, scan every class in this package (com.wcci.reviews) and subpackages for
+		// Spring-related annotations. Once Spring has identified every component which have dependencies
+		// and every component providing such dependencies, it hooks them altogether and runs the program.
 		SpringApplication.run(ReviewsApplication.class, args);
 	}
 
+	// Hey, Spring, here's a "CommonsRequestLoggingFilter" implementation, which will log http requests like I want.
+	// I expect that the RestController traffic will therefore be logged conveniently.
 	@Bean
 	public CommonsRequestLoggingFilter requestLoggingFilter() {
 		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
