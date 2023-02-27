5e0b6765835334bbfd05c9cce8da469199ba8894
Marshall F <marshall@wecancodeit.org>
2023-02-27 10:19:51 -0500

    rename endpoint, backwards-compatible

--- src/main/java/com/wcci/reviews/templateControllers/Summary.java
+++ src/main/java/com/wcci/reviews/templateControllers/Summary.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// What is the *job* of this class?
+// It's basically:
+//   * To know which template should be used for which endpoint.
+//   * How to populate the model with the information that template needs
 @Controller
 public class Summary {
     final private CategoryRepository categoryRepository;
@@ -14,10 +18,11 @@ public class Summary {
         this.categoryRepository = categoryRepository;
     }
 
-
-    @GetMapping("/summary.html")
+    // Yes, a better endpoint would called /categorySummary.html, but somebody already told the public that the data
+    // was available at /summary.html so we can't stop supporting it now.
+    @GetMapping(path = {"/summary.html", "/categorySummary.html"})
     public String displaySummary(final Model model) {
         model.addAttribute("categories", categoryRepository.findAll());
-        return "summary";
+        return "categorySummary";
     }
 }
